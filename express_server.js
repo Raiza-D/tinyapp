05f802c3cf24cd90c5b232454ad3481180511ebd
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-28 21:28:01 -0700

    Remove commented notes.

--- express_server.js
+++ express_server.js
@@ -30,7 +30,7 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars); // Show a certain view --> render. Pass variables into the view.
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -41,12 +41,7 @@ app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = longURL;
-  res.redirect(`urls/${shortURL}`); // How did it know to use the urls_show template?
-  // Line 44 is the same as running code within app.get("/urls/:shortURL").
-  // When code jumps to Line 44, it will know to run app.get("/urls/:shortURL") because
-  // of this part within Line 44: `urls/${shortURL}`
-  // console.log(urlDatabase);
-  // res.redirect("/urls"); // To be changed (Part 2)
+  res.redirect(`urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -60,26 +55,9 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-
-    if (!longURL) { // longURL === undefined
-      console.log("Cannot render page. Invalid shortURL");
-      return res.redirect("http://www.google.com");
-    }
-  res.redirect(longURL); // res.redirect(/error) This will result in code jumping to Line 68.
-});
-
-app.get("/error", (req, res) => {
-  res.render()// Create new  ejs template for an error). Or res.redirect to main page.
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-/* To handle error of user inputs an invalid shortURL:
-Path of ("/u/") render an error page that shows error message.
-Path of ("u/someinvalidshorturl") render error page.
-Rendering error page requires creating a new ejs template for handling errors and this page will
-display.
-Or re-direct to another page within app. Better to render an error page.
-*/
