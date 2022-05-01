d870d5622163fae65f188379e9e8baf4671b7031
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-01 12:44:58 -0700

    Add code logic to handle request when user clicks on logout button. User is redirected to /urls page. Have to fix issue of shortURL and longURL still visible after logout. There shouldn't be content.

--- express_server.js
+++ express_server.js
@@ -101,6 +101,12 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+// Handles request to logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
