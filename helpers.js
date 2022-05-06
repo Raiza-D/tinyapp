cdab187c41ed2ad127ed039dc116774e06b044bc
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-06 11:14:40 -0700

    Switch parameters around for getUserByEmail function. Before I had users, email. Then switched it so email comes first as the first param, followed by the users object. Compass was easier to follow, that's why I made this change. They had the params listed in this order. Had to change my express_server.js file accordingly where this function was being invoked.

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@
 /* Function scans users object. Checks if email entered upon registering or logging in exists.
 If yes, return user nested object within users database.
 If no, value is 'undefined' */
-const getUserByEmail = function(users, email) {
+const getUserByEmail = function(email, users) {
   for (const user in users) {
     if (users[user].email === email) {
       return users[user];
