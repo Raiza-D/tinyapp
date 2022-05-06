c1c9db2bf154da960a9eb07356b8400e39b03f34
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-05 21:38:49 -0700

    Cut and pasted getUserByEmail function into newly created helpers.js file. Wrote code to require getUserByEmail function from helpers.js file.

--- express_server.js
+++ express_server.js
@@ -24,21 +24,10 @@ app.use(
 
 const bcrypt = require("bcryptjs");
 
+const { getUserByEmail } = require("./helpers");
+
 const generateRandomString = function() {
   return (Math.random() + 1).toString(36).substring(6);
-}
-
-/* Function scans users object. Checks if email entered upon registering or logging in exists.
-If yes, return user nested object within users database.
-If no, value is 'undefined' */
-const getUserByEmail = function(users, email) {
-  for (const user in users) {
-    if (users[user].email === email) {
-      return users[user];
-    }
-  }
-
-  return null;
 }
 
 /* Function to authenticate user when logging in. Loops through users object.
