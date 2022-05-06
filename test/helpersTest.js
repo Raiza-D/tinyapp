e1ea1706614ac6677aee41c266c73d42f25c76da
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-05 22:24:50 -0700

    1) Add code to require chai and its assertion library. 2)Add code to require getUserByEmail function from helpers.js in project's root directory. 3)Add testUsers object as sample data to use in our tests. 4)Add test to check scenario if a user with a valid email exists. Should return user with that valid email. 5)Write a second test to check scenario when email provided does not exist in database, therefore, user does not exist.

---
+++ test/helpersTest.js
@@ -0,0 +1,31 @@
+const { assert } = require("chai");
+
+const { getUserByEmail } = require("../helpers.js");
+
+const testUsers = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+describe("getUserByEmail", function() {
+  it("should return a user with a valid email", function() {
+    const user = getUserByEmail(testUsers, "user@example.com");
+    const expectedUserID = "userRandomID";
+    assert.deepEqual(user, expectedUserID);
+  });
+
+  it("should return null if the email does not exist within our users database", function() {
+    const user = getUserByEmail(testUsers, "someperson@gmail.com");
+    const expectedOutput = null;
+    assert.equal(user, expectedOutput);
+  });
+
+});
